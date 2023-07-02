955134a82d3acf581460e8dfd3c3ffdd355936ce
Andres L <>
2023-07-02 10:43:06 +0100

    It was added the Open API configuration

--- Readme.md
+++ Readme.md
@@ -1,5 +1,3 @@
-En la base de datos de comercio electrónico de la compañía disponemos de la tabla PRICES que refleja el precio final (pvp) y la tarifa que aplica a un producto de una cadena entre unas fechas determinadas. A continuación se muestra un ejemplo de la tabla con los campos relevantes:
-
 PRICES
 -------
 
@@ -20,26 +18,6 @@ PRIORITY: Desambiguador de aplicación de precios. Si dos tarifas coinciden en u
 PRICE: precio final de venta.
 CURR: iso de la moneda.
 
-Se pide:
-
-Construir una aplicación/servicio en SpringBoot que provea una end point rest de consulta  tal que:
-
-Acepte como parámetros de entrada: fecha de aplicación, identificador de producto, identificador de cadena.
-Devuelva como datos de salida: identificador de producto, identificador de cadena, tarifa a aplicar, fechas de aplicación y precio final a aplicar.
-
-Se debe utilizar una base de datos en memoria (tipo h2) e inicializar con los datos del ejemplo, (se pueden cambiar el nombre de los campos y añadir otros nuevos si se quiere, elegir el tipo de dato que se considere adecuado para los mismos).
-
-Desarrollar unos test al endpoint rest que  validen las siguientes peticiones al servicio con los datos del ejemplo:
-
--          Test 1: petición a las 10:00 del día 14 del producto 35455   para la brand 1 (ZARA)
--          Test 2: petición a las 16:00 del día 14 del producto 35455   para la brand 1 (ZARA)
--          Test 3: petición a las 21:00 del día 14 del producto 35455   para la brand 1 (ZARA)
--          Test 4: petición a las 10:00 del día 15 del producto 35455   para la brand 1 (ZARA)
--          Test 5: petición a las 21:00 del día 16 del producto 35455   para la brand 1 (ZARA)
 
 
-Se valorará:
-
-Diseño y construcción del servicio.
-Calidad de Código.
-Resultados correctos en los test.
+http://localhost:8080/inditex/swagger-ui/index.html
--- src/main/java/com/shop/inditex/web/controller/PriceController.java
+++ src/main/java/com/shop/inditex/web/controller/PriceController.java
@@ -2,13 +2,22 @@ package com.shop.inditex.web.controller;
 
 import com.shop.inditex.domain.dto.PriceDto;
 import com.shop.inditex.domain.service.PriceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
@@ -23,8 +32,16 @@ public class PriceController {
 
 
     @GetMapping("/get-prices")
-    public ResponseEntity<PriceDto> getPrices(@RequestParam(value = "applicationDate", required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date,
+    @Operation(summary = "Get the best price among the dates")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the best price", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Price not found", content = @Content)})
+    public ResponseEntity<PriceDto> getPrices(@Parameter(description = "Date", example = "2020-10-12T07:30:10Z")
+                                              @RequestParam(value = "applicationDate", required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date,
+                                              @Parameter(description = "productId", example = "35455")
                                               @RequestParam(value = "productId", required = true) Integer productId,
+                                              @Parameter(description = "brandId", example = "1 (ZARA)")
                                               @RequestParam(value = "brandId", required = true) Integer brandId) {
         LOGGER.info("PriceController::getPrices {} ", date, productId, brandId);
         return new ResponseEntity<>(priceService.getCorrectPrice(date, productId, brandId), HttpStatus.OK);
