ce2ce78fc4ac322d32c644de9b2d39833cf55d71
Andres L <>
2023-07-21 16:02:08 +0100

    It was added JWT

--- src/main/java/com/shop/inditex/web/config/SecurityConfig.java
+++ src/main/java/com/shop/inditex/web/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.shop.inditex.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,9 +18,7 @@ public class SecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(customizeRequests -> customizeRequests
-                         //.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
-                        .requestMatchers(HttpMethod.POST).denyAll()
-                        .requestMatchers(HttpMethod.PUT).denyAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE).denyAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
@@ -26,6 +26,11 @@ public class SecurityConfig  {
                 .cors(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
